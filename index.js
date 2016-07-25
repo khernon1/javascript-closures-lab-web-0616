52ec9ee76dd20a0fc2ff55d9daeba5b17316c36b
Kevin Hernon <khernon1@gmail.com>
2016-07-25 19:05:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+
+  return {
+    deadlyDevice,
+    sharkCreator
+  }
+}
+
+var sharkCreator = createAnimal('Shark')  
+
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
